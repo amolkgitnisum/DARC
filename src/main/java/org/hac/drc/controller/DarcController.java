d7d0eb599b5695937f0f6aaf3175f34606d46fdb
user <user@dell>
2017-11-23 16:50:23 +0530

    added path variable

--- src/main/java/org/hac/drc/controller/DarcController.java
+++ src/main/java/org/hac/drc/controller/DarcController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.hac.drc.dao.DarcDaoImpl;
 import org.hac.drc.service.DarcService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,9 @@ public class DarcController {
 	DarcDaoImpl darcDaoImpl;
 	
 	@RequestMapping(value="generatereport",method=RequestMethod.GET)
-	public Map generateReport() throws Exception
+	public Map generateReport(@PathVariable("url") String url) throws Exception
 	{
-		String url = "https://www.macys.com";
+		//String url = "https://www.macys.com";
 		Map status=darcDaoImpl.getfinalresult(url);
 		
 		return status;
